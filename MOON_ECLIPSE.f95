0fb11d20ddccc6aa4f46c8e5cc251ea142822d25
Jason Ding <760837268@qq.com>
2018-11-24 08:03:58 +0800

    new
    new

---
+++ MOON_ECLIPSE.f95
@@ -0,0 +1,211 @@
+PROGRAM MOON_ECLIPSE
+  IMPLICIT NONE
+
+! MOON_ECLIPSE.f95 :Compute Partial Eclipse begins Time(U1)
+! 
+!          Copyright (C) 2018 by DING Junsheng, All rights reserved.
+! 
+! version :$Revision: 1.1 $ $Date: 2018/11/25 $
+! history : 2018/11/25  1.0  new
+
+  REAL *8 :: AU                             !Number of kilometers per astronomical unit (km)
+  REAL *8 :: CLIGHT                         !Speed of light (km/s)
+  REAL *8 :: ASUN                           !Radius of the sun (km)
+  REAL *8 :: AM                             !Radius of the moon (km)
+  REAL *8 :: RE                             !Radius of the earth's equator (km)
+  INTEGER :: DENUM                          !Vrision of DE EPHEMERIS
+
+  INTEGER :: NCENT                          !INTEGER NUMBER OF CENTER POINT
+  INTEGER :: NTARG                          !INTEGER NUMBER OF 'TARGET' POINT
+  INTEGER :: YEAR                           !year
+  INTEGER :: MONTH                          !month
+  INTEGER :: DAY                            !day
+  REAL *8 :: DDAY                           !the fraction of a day
+  INTEGER :: HOUR                           !hour
+  INTEGER :: MINUTE                         !minute
+  REAL *8 :: SECOND                         !second
+  REAL *8 :: DJM0                           !MJD zero-point: always 2400000.5
+  REAL *8 :: DJM                            !Modified Julian Date for 0 hrs
+
+  REAL *8,DIMENSION(6) :: RRD               !6-WORD D.P. ARRAY CONTAINING POSITION AND VELOCITY OF POINT 'NTARG' RELATIVE TO 'NCENT'
+  REAL *8,DIMENSION(3) :: STE               !ARRAY OF 'EARTH' RELATIVE TO 'SUN'
+  REAL *8,DIMENSION(3) :: ETO               !ARRAY OF 'Shadow cone' RELATIVE TO 'EARTH'
+  REAL *8,DIMENSION(3) :: ETM               !ARRAY OF 'EARTH' RELATIVE TO 'MOON'
+  REAL *8,DIMENSION(3) :: MTO               !ARRAY OF 'Shadow cone' RELATIVE TO 'MOON'
+  REAL *8,DIMENSION(3) :: STM               !ARRAY OF 'MOON' RELATIVE TO 'SUN'
+
+  REAL *8 :: ANGLE_SEM                      !ANGLE OF Sun-Earth-Moon
+  REAL *8 :: ANGLE_EOM                      !ANGLE OF Earth-ShadowCone-Moon
+  REAL *8 :: ANGLE_EO                       !ANGLE OF Earth-ShadowCone
+  REAL *8 :: ANGLE_MO                       !ANGLE OF Moon-ShadowCone
+
+  REAL *8 :: PRODUCT                        !inner product 
+
+  REAL *8 :: DT0                            !light time lteration variable 
+  REAL *8 :: DT1                            !light time lteration variable
+  REAL *8 :: vals                           !constant storage
+  CHARACTER*6 :: nams                       !constant name
+  REAL *8 :: ERR                            !angle error
+  REAL *8 :: iteration_val=1.0/(24*3600)    !1 second to DJM
+  INTEGER :: NDP=3                          !number of decimal places of seconds
+  INTEGER ,DIMENSION(4) :: IHMSF            !hours, minutes, seconds, fraction
+  CHARACTER :: SIGN                         !'+' or '-'
+
+  INTEGER :: J                              !status: 0 = OK
+
+! Read constants from the ephemeris file ***************************************
+
+  WRITE(*,80)
+  80 FORMAT(//,'CONSTANTS...',/)
+
+  nams='AU'
+    CALL selconQ(nams,vals)
+    WRITE(*,'(A9,F20.3)')nams,vals
+    AU=vals
+
+  nams='AM'
+    CALL selconQ(nams,vals)
+    WRITE(*,'(A9,F20.3)')nams,vals
+    AM=vals/AU
+
+  nams='RE'
+    CALL selconQ(nams,vals)
+    WRITE(*,'(A9,F20.3)')nams,vals
+    RE=(vals+65D0)/AU
+
+  nams='ASUN'
+    CALL selconQ(nams,vals)
+    WRITE(*,'(A9,F20.3)')nams,vals
+    ASUN=vals/AU
+
+  nams='DENUM'
+    CALL selconQ(nams,vals)
+    WRITE(*,'(A9,F20.3)')nams,vals
+    DENUM=vals
+
+  nams='CLIGHT'
+    CALL selconQ(nams,vals)
+    WRITE(*,'(A9,F20.3)')nams,vals
+    CLIGHT=vals/AU
+
+! Locate the most likely day *******************************************************
+
+    YEAR  = 2019
+    MONTH = 1
+    DAY   = 1
+    CALL iau_CAL2JD ( YEAR, MONTH, DAY, DJM0, DJM, J )
+
+    DO
+
+    NCENT = 11     !the sun
+    NTARG = 3      !the earth
+
+    CALL PLEPH(DJM0+DJM, NCENT, NTARG, RRD)
+
+    ! vector of the sun to the earth
+    STE(1) = RRD(1)                      
+    STE(2) = RRD(2)
+    STE(3) = RRD(3)
+
+    NCENT  = 10    ! the moon
+    NTARG  = 3     ! the earth
+    CALL PLEPH(DJM0+DJM, NCENT, NTARG, RRD)
+
+    ! vector of the moon the to earth
+    ETM(1) = RRD(1)            
+    ETM(2) = RRD(2)
+    ETM(3) = RRD(3)
+
+    CALL iau_PDP (STE,ETM,PRODUCT)
+    ANGLE_SEM = ACOS(PRODUCT/(SQRT(STE(1)*STE(1)+STE(2)*STE(2)+STE(3)*STE(3))*SQRT(ETM(1)*ETM(1)+ETM(2)*ETM(2)+ETM(3)*ETM(3))))
+    !WRITE(*,*)ANGLE_SEM
+
+    IF(ANGLE_SEM.LE.2.92116) THEN
+      DJM = DJM + 1.0
+    ELSE IF(ANGLE_SEM.LE.3.01765) THEN
+      DJM = DJM + 0.5
+    ELSE
+      EXIT
+    END IF
+
+    END DO
+
+! Main body********************************************************************************
+
+  DO
+    NCENT = 11     !the sun
+    NTARG = 3      !the earth
+
+! Compute light time **********************************************************************
+
+    CALL PLEPH(DJM0+DJM, NCENT, NTARG, RRD)
+    DT1 = ((SQRT(RRD(1)*RRD(1)+RRD(2)*RRD(2)+RRD(3)*RRD(3))-ASUN-RE)*iteration_val)/CLIGHT
+    DT0 = 0.
+
+    DO WHILE(ABS(DT0-DT1)>1.16E-8)
+    DT0 = DT1
+    CALL PLEPH(DJM0+DJM-DT0, NCENT, NTARG, RRD)
+    DT1 = ((SQRT(RRD(1)*RRD(1)+RRD(2)*RRD(2)+RRD(3)*RRD(3))-ASUN-RE)*iteration_val)/CLIGHT
+    END DO
+
+! Compute angles ***************************************************************************
+     
+    ! vector of the sun to the earth
+    STE(1) = RRD(1)                      
+    STE(2) = RRD(2)
+    STE(3) = RRD(3)
+
+    ! vector of the earth to the cone
+    ETO(1) = (RE/(RE-ASUN))*STE(1)      
+    ETO(2) = (RE/(RE-ASUN))*STE(2)
+    ETO(3) = (RE/(RE-ASUN))*STE(3)
+
+    NCENT  = 10    ! the moon
+    NTARG  = 3     ! the earth
+    CALL PLEPH(DJM0+DJM, NCENT, NTARG, RRD)
+
+    ! vector of the moon the to earth
+    ETM(1) = RRD(1)            
+    ETM(2) = RRD(2)
+    ETM(3) = RRD(3)
+
+    ! vector of the moon the to cone
+    MTO(1) = ETO(1) - ETM(1)    
+    MTO(2) = ETO(2) - ETM(2)
+    MTO(3) = ETO(3) - ETM(3)
+
+    ! angles
+    CALL iau_PDP (ETO,MTO,PRODUCT)     
+    ANGLE_EOM = ACOS(PRODUCT/(SQRT(ETO(1)*ETO(1)+ETO(2)*ETO(2)+ETO(3)*ETO(3))*SQRT(MTO(1)*MTO(1)+MTO(2)*MTO(2)+MTO(3)*MTO(3))))
+    ANGLE_EO  = ATAN(RE/SQRT(ETO(1)*ETO(1)+ETO(2)*ETO(2)+ETO(3)*ETO(3)))
+    ANGLE_MO  = ATAN(AM/SQRT(MTO(1)*MTO(1)+MTO(2)*MTO(2)+MTO(3)*MTO(3)))
+
+    ! angle error
+    ERR = ANGLE_EOM - ANGLE_EO - ANGLE_MO     
+ 
+    ! DJM plus 1s
+    DJM = DJM+iteration_val               
+  
+    ! loop exit condition
+    IF(ERR.LE.0D0) EXIT                
+
+  END DO
+
+! Output the results ***************************************************
+
+  WRITE(*,85)
+  85 FORMAT(//,'RESULTS...')
+  WRITE(*,90)DT1/iteration_val
+  90 FORMAT(/,' LIGHT TIME: ',F15.3,' second',/)
+
+  CALL iau_JD2CAL ( DJM0, DJM, YEAR, MONTH, DAY, DDAY, J )
+  DDAY=DDAY-(32.164+37)*iteration_val                       
+  CALL sla_DD2TF (NDP, DDAY, SIGN, IHMSF)                    
+  WRITE(*,*)'Partial Eclipse begins(U1)'
+  WRITE(*,100) YEAR,MONTH,DAY,IHMSF(1),IHMSF(2),IHMSF(3)+IHMSF(4)/1000.0   
+  100 FORMAT(1X,I4,'-',I2,'-',I2,' UTC Time:',I2,':',I2,':',F6.3)
+  WRITE(*,110)ANGLE_EOM,ANGLE_EO,ANGLE_MO
+  110 FORMAT(' ANGLE_EOM =',F10.6/,' ANGLE_EO  =',F10.6/,' ANGLE_MO  =',F10.6/)
+
+  END PROGRAM MOON_ECLIPSE
+
---
+++ README.md
@@ -0,0 +1,244 @@
+#### README
+
+THIS IS A READ ME OF PROGRAM MOON_ECLIPSE
+
+
+
+##### mainfiles
+
+1. MOON_ECLIPSE.f95              !主函数文件
+2. README.md                           !说明文件
+3. SOFALIB.f                                !SOFA及其它官方提供的子程序集
+4. JPLEPH                                     !二进制历表文件
+5. selcon.f                                    !DE历表测试文件
+
+
+
+##### principles and strategies
+
+1. 下图为月食发生前时刻地月日空间关系，
+
+![](/media/jason/新加卷/UCAS/课程/天体测量学及其应用/H3月食作业/原理图.png)
+
+<center>Figure 1. 月食发生前时刻地月日空间关系示意图</center>
+
+Figure 2. 中，S为太阳，E为地球，M为月球，O为地球本影影锥。初亏时刻，存在矢量OE和OM的夹角∠MOE等于视差角ω1和视差角ω2之和（视O为stars，E和M为公转轨道）。
+
+
+
+2. 由于太阳光从太阳出发到经过地球形成影锥需要一定时间，故需要计算光行时。下图为计算光行时的示意图。
+
+![](/media/jason/新加卷/UCAS/课程/天体测量学及其应用/H3月食作业/光行时.png)
+
+<center>Figure 2. 迭代求解光行时</center>
+
+步骤为：
+
+1. 从给定T0时刻的历表⼏何位置，计算观测者⾄被测体的距离L0，估算光⾏时DT0；
+2. 基于被测天体运动速度计算T0+DT0时刻新位置，重新计算观测者⾄被测体的距离L1，估算光⾏时DT1；
+3. 基于被测天体运动速度计算T0+DT1时刻新位置，重新计算观测者⾄被测体的距离L2，估算光⾏时DT2；
+4. 当 | DTn - DTn-1 | 近似等于0时, 即计算出了精准的光⾏时DTn（n>=1）,进⽽可计算出被测体准确视位置。
+
+
+
+3. 加快计算过程采用的策略
+
+   -- 由于月食发生时日-地-月成一条直线，此时日-地-月所成夹角靠近180度，故首先以一天为步长，
+
+   过滤夹角角度过大的时间，定位出月食发生的那一天。
+
+   -- 可根据影锥大致角度，继续以半天或一小时为步长逼近，程序仅使用到半天，可根据需要缩小。
+
+
+
+4. 程序计算结果需要有参考，挪威斯塔万格的Tmeanddate公司给出的2019年第一次月食发生时间如下图：
+
+![](/media/jason/%E6%96%B0%E5%8A%A0%E5%8D%B7/UCAS/%E8%AF%BE%E7%A8%8B/%E5%A4%A9%E4%BD%93%E6%B5%8B%E9%87%8F%E5%AD%A6%E5%8F%8A%E5%85%B6%E5%BA%94%E7%94%A8/H3%E6%9C%88%E9%A3%9F%E4%BD%9C%E4%B8%9A/%E5%8F%82%E8%80%83%E6%A0%87%E5%87%86.png)
+
+<center>Figure 3. 月食参考时间</center>
+
+图片来源：[https://www.timeanddate.com/eclipse/lunar/2019-january-21](https://www.timeanddate.com/eclipse/lunar/2019-january-21)
+
+
+
+5. 程序运行结果如下，与Figure 3. 符合程度较佳
+
+![](/media/jason/新加卷/UCAS/课程/天体测量学及其应用/H3月食作业/2018-11-24 15-35-24屏幕截图.png)
+
+
+
+##### subroutines
+
+以下列出了程序调用的子程序：
+
+```fortran
+SUBROUTINE iau_CAL2JD ( IY, IM, ID, DJM0, DJM, J )
+```
+
+1. **所在文件**：cal2jd.for
+2. **功能说明**：格里高历转为儒略历
+3. **参数说明**：
+
+```
+*  Given:
+*     IY,IM,ID    i     year, month, day in Gregorian calendar (Note 1)
+*
+*  Returned:
+*     DJM0        d     MJD zero-point: always 2400000.5
+*     DJM         d     Modified Julian Date for 0 hrs
+*     J           i     status:
+*                           0 = OK
+*                          -1 = bad year   (Note 3: JD not computed)
+*                          -2 = bad month  (JD not computed)
+*                          -3 = bad day    (JD computed)
+```
+
+
+
+```fortran
+SUBROUTINE iau_JD2CAL ( DJ1, DJ2, IY, IM, ID, FD, J )
+```
+
+1. **所在文件**：jd2cal.for
+2. **功能说明**：儒略历转为格里高历
+3. **参数说明**：
+
+```
+*  Given:
+*     DJ1,DJ2     d     Julian Date (Notes 1, 2)
+*
+*  Returned:
+*     IY          i     year
+*     IM          i     month
+*     ID          i     day
+*     FD          d     fraction of day
+*     J           i     status:
+*                           0 = OK
+*                          -1 = unacceptable date (Note 1)
+```
+
+
+
+```fortran
+SUBROUTINE iau_PDP ( A, B, ADB )
+```
+
+1. **所在文件**：pdp.for
+2. **功能说明**：向量做点积
+3. **参数说明**：
+
+```
+Given:
+*     A        d(3)      first p-vector
+*     B        d(3)      second p-vector
+*
+*  Returned:
+*     ADB      d         A . B
+```
+
+
+
+```fortran
+SUBROUTINE sla_DD2TF (NDP, DAYS, SIGN, IHMSF)
+```
+
+1. **所在文件**：selcon.f
+2. **功能说明**：天到时分秒
+3. **参数说明**：
+
+```
+*  Given:
+*     NDP       int      number of decimal places of seconds
+*     DAYS      dp       interval in days
+*
+*  Returned:
+*     SIGN      char     '+' or '-'
+*     IHMSF     int(4)   hours, minutes, seconds, fraction
+```
+
+
+
+```fortran
+subroutine selconQ(nams,vals)
+```
+
+1. **所在文件**：selcon.f
+2. **功能说明**：常量提取
+3. **参数说明**：
+
+```
+c  Input 
+
+c           nams :  names (character*6)
+
+c  Output
+
+c           vals :  values corresponding to the input names
+C
+C   QI-ZHAOXIANG@20130402
+
+```
+
+
+
+```fortran
+SUBROUTINE PLEPH ( ET, NTARG, NCENT, RRD )
+```
+
+1. **所在文件**：selcon.f
+2. **功能说明**：返回给定时间中心天体到目标天体的矢量
+3. **参数说明**：
+
+```
+C     THIS SUBROUTINE READS THE JPL PLANETARY EPHEMERIS
+C     AND GIVES THE POSITION AND VELOCITY OF THE POINT 'NTARG'
+C     WITH RESPECT TO 'NCENT'.
+C
+C     CALLING SEQUENCE PARAMETERS:
+C
+C       ET = D.P. JULIAN EPHEMERIS DATE AT WHICH INTERPOLATION
+C            IS WANTED.
+C
+C       ** NOTE THE ENTRY DPLEPH FOR A DOUBLY-DIMENSIONED TIME **
+C          THE REASON FOR THIS OPTION IS DISCUSSED IN THE
+C          SUBROUTINE STATE
+C
+C     NTARG = INTEGER NUMBER OF 'TARGET' POINT.
+C
+C     NCENT = INTEGER NUMBER OF CENTER POINT.
+C
+C            THE NUMBERING CONVENTION FOR 'NTARG' AND 'NCENT' IS:
+C
+C                1 = MERCURY           8 = NEPTUNE
+C                2 = VENUS             9 = PLUTO
+C                3 = EARTH            10 = MOON
+C                4 = MARS             11 = SUN
+C                5 = JUPITER          12 = SOLAR-SYSTEM BARYCENTER
+C                6 = SATURN           13 = EARTH-MOON BARYCENTER
+C                7 = URANUS           14 = NUTATIONS (LONGITUDE AND OBLIQ)
+C                            15 = LIBRATIONS, IF ON EPH FILE
+C
+C             (IF NUTATIONS ARE WANTED, SET NTARG = 14. FOR LIBRATIONS,
+C              SET NTARG = 15. SET NCENT=0.)
+C
+C      RRD = OUTPUT 6-WORD D.P. ARRAY CONTAINING POSITION AND VELOCITY
+C            OF POINT 'NTARG' RELATIVE TO 'NCENT'. THE UNITS ARE AU AND
+C            AU/DAY. FOR LIBRATIONS THE UNITS ARE RADIANS AND RADIANS
+C            PER DAY. IN THE CASE OF NUTATIONS THE FIRST FOUR WORDS OF
+C            RRD WILL BE SET TO NUTATIONS AND RATES, HAVING UNITS OF
+C            RADIANS AND RADIANS/DAY.
+C
+C            The option is available to have the units in km and km/sec.
+C            For this, set km=.true. in the STCOMX common block.
+```
+
+
+
+##### getcode
+
+this project：
+
+SOFA：[http://www.iausofa.org/index.html](http://www.iausofa.org/index.html)
+
+DE/LE EPHEMERIS：[ftp://ssd.jpl.nasa.gov](ftp://ssd.jpl.nasa.gov)
+
